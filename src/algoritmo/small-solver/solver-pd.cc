a4947d8203669e1753557ac1452faca326876107
AlbertoDelCastillo <alu0101627137@ull.edu.es>
2026-03-15 19:14:03 +0000

    fix: Arreglar inilizacion de tabla para slots expandidos

--- src/algoritmo/small-solver/solver-pd.cc
+++ src/algoritmo/small-solver/solver-pd.cc
@@ -35,36 +35,50 @@
  * @throws std::invalid_argument Si la instancia no es de tipo InstanciaEmpleados.
  */
 Solucion* SolverPD::ResolverDiaPequeño(Instancia* inst) {
-	InstanciaEmpleados* inst_emp = dynamic_cast<InstanciaEmpleados*>(inst);
-	if (inst_emp == nullptr) {
-		throw std::invalid_argument("Error: la instancia no es de tipo InstanciaEmpleados");
-	}
-	int num_emp = inst_emp->GetNumEmpleados();
-	int num_turnos = inst_emp->GetNumTurnos();
-	SolucionEmpleados* sol = new SolucionEmpleados(num_emp, 1, 0, 0);
-	std::vector<std::vector<int>> tabla_puntuaciones(
-			num_emp + 1, std::vector<int>(num_turnos + 1, 0));
-	std::vector<std::vector<bool>> asignado(
-			num_emp + 1, std::vector<bool>(num_turnos + 1, false));
-	for (int emp_actual = 1; emp_actual <= num_emp; ++emp_actual) {
-		for (int turnos_actual = 1; turnos_actual <= num_turnos; ++turnos_actual) {
-			int sat_no_asignar = tabla_puntuaciones[emp_actual - 1][turnos_actual];
-			int sat_asignar = tabla_puntuaciones[emp_actual - 1][turnos_actual - 1] +
-					inst_emp->GetSatisfaccion(emp_actual - 1, 0, turnos_actual - 1);
-			if (sat_asignar > sat_no_asignar) {
-				tabla_puntuaciones[emp_actual][turnos_actual] = sat_asignar;
-				asignado[emp_actual][turnos_actual] = true;
-			} else {
-				tabla_puntuaciones[emp_actual][turnos_actual] = sat_no_asignar;
-			}
-		}
-	}
-	int turnos_actual = num_turnos;
-	for (int emp_actual = num_emp; emp_actual > 0 && turnos_actual > 0; --emp_actual) {
-		if (asignado[emp_actual][turnos_actual]) {
-			sol->SetAsignacion(emp_actual - 1, 0, turnos_actual - 1);
-			--turnos_actual;
-		}
-	}
-	return sol;
+  InstanciaEmpleados* inst_emp = dynamic_cast<InstanciaEmpleados*>(inst);
+  if (inst_emp == nullptr) {
+    throw std::invalid_argument("Error: la instancia no es de tipo InstanciaEmpleados");
+  }
+  int num_emp    = inst_emp->GetNumEmpleados();
+  int num_turnos = inst_emp->GetNumTurnos();
+  int dia        = 0;
+  std::vector<int> slots;
+  for (int t = 0; t < num_turnos; ++t) {
+    int min_emp = inst_emp->GetMinTurnos(dia, t);
+    for (int p = 0; p < min_emp; ++p) {
+      slots.push_back(t);
+    }
+  }
+  int K = slots.size();
+  SolucionEmpleados* sol = new SolucionEmpleados(num_emp, 1, 0, 0);
+  if (K == 0) {
+    return sol;
+  }
+  std::vector<std::vector<int>> tabla(
+      num_emp + 1, std::vector<int>(K + 1, 0));
+  std::vector<std::vector<bool>> asignado(
+      num_emp + 1, std::vector<bool>(K + 1, false));
+  for (int e = 1; e <= num_emp; ++e) {
+    for (int k = 1; k <= K; ++k) {
+      int turno          = slots[k - 1];
+      int sat_no_asignar = tabla[e - 1][k];
+      int sat_asignar    = tabla[e - 1][k - 1] +
+                           inst_emp->GetSatisfaccion(e - 1, dia, turno);
+      if (sat_asignar > sat_no_asignar) {
+        tabla[e][k]    = sat_asignar;
+        asignado[e][k] = true;
+      } else {
+        tabla[e][k] = sat_no_asignar;
+      }
+    }
+  }
+  int k = K;
+  for (int e = num_emp; e > 0 && k > 0; --e) {
+    if (asignado[e][k]) {
+      int turno = slots[k - 1];
+      sol->SetAsignacion(e - 1, dia, turno);
+      --k;
+    }
+  }
+  return sol;
 }
